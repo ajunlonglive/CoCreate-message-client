5cd73703cec7443ab33b738d8381015b0b45f518
frank <frank@cocreate.app>
2021-04-30 04:26:09 +0000

    add src

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "path": "./docs/index.html",
             "collection": "files",
             "document_id": "6060b7e5c759fc34e923ce92",
-            "key": "html",
+            "key": "src",
             "data":{
                 "name": "message-client Doc",
                 "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
